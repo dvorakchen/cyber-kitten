eb713e924f166f62a55bb50ff167258b565e9799
dvorak <dvorakchen@outlook.com>
2024-02-14 23:44:30 +0800

    perf: compress audio files

--- src/cats.rs
+++ src/cats.rs
@@ -97,12 +97,12 @@ pub fn CatSit() -> impl IntoView {
     let for_purr_start = Rc::new(purr);
     let for_purr_end = Rc::clone(&for_purr_start);
 
-    let (index, set_index) = create_signal(0);
+    let (index, set_index) = create_signal(0isize);
     let mut flag: isize = -1;
 
     Interval::new(500, move || {
         match index.get_untracked() {
-            0 | 4 => flag = -flag,
+            v if v == 0 || v == (CAT_SIT.len() - 1) as isize => flag = -flag,
             _ => {}
         };
         set_index.update(|v| *v += flag);
@@ -129,16 +129,16 @@ pub fn CatSit() -> impl IntoView {
 
     view! {
         <div
-            class="cursor-grab select-none"
+            class="cursor-grab select-none scale-100 sm:scale-150 lg:scale-[200%]"
             on:click=handle_click_cat
             on:pointerdown=handle_touch_cat
             on:pointerup=handle_untouch_cat
         >
-            <div class="scale-100 sm:scale-150 lg:scale-[200%]">
+            <div class="p-2">
                 {move || { get_cat_sit(CAT_SIT[index.get() as usize]) }}
             </div>
-            <audio src="/assets/cat_voice.mp3" hidden node_ref=meow></audio>
-            <audio src="/assets/cat_purr.mp3" hidden node_ref=purr></audio>
+            <audio src="/assets/cat_meow.ogg" hidden node_ref=meow></audio>
+            <audio src="/assets/cat_purr.ogg" hidden node_ref=purr></audio>
         </div>
     }
 }
